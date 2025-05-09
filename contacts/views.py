854d9b6b2033a336bed5be684a144e8bbcc5dcbd
Yamina Guenez <yaminaguenez@gmail.com>
2025-05-09 23:45:14 +0100

    Form Errors

--- contacts/views.py
+++ contacts/views.py
@@ -29,7 +29,7 @@ def search_contacts(request):
 @login_required
 @require_http_methods(["POST"])
 def create_contact(request):
-    form =  ContactForm(request.POST)
+    form =  ContactForm(request.POST,  initial={'user': request.user})
     if form.is_valid():
         contact = form.save(commit=False)
         contact.user = request.user
@@ -38,3 +38,11 @@ def create_contact(request):
         response = render(request, "partials/contact-row.html", context)
         response["HX-Trigger"] = "success"
         return response
+
+    else:
+         response = render(request, "partials/add-contact-modal.html", {'form': form})
+         response['HX-Retarget']= '#contact_modal'
+         response['HX-Reswap']= 'outerHTML'
+         response['HX-Trigger-After-Settle']= 'fail'
+         
+         return response
